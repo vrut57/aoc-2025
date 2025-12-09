ad3b767b339370b1436495e5c081e82faadf0d2c
vrut57 <vrut57@gmail.com>
2025-12-08 22:50:52 -0500

    Day7 - part 2

--- day7/laboratories.js
+++ day7/laboratories.js
@@ -15,7 +15,6 @@ async function processInput(filepath) {
 }
 
 function part1(input) {
-    console.log(input);
     let count = 0;
     let beamIndices = [];
     input.forEach((ele, i) => {
@@ -24,35 +23,35 @@ function part1(input) {
         if(beamIndices.length === 0) {
             beamIndices.push([input[0].split("").indexOf("S")])
         } else {
-            // console.log({beamIndices})
             beamIndices[i - 1].forEach((x) => {
-                // console.log({beamIndices})
-                // const beamIdx = beamIndices[i - 1];
-                    // console.log({beamIdx})
-                    // console.log({i, x})
                     if(row[x] === ".") {
-                        // keep idx, no change
-                        // console.log("No change")
-                        // beamIndices.push([...x])
                         newIndexs.push(x);
                     } else if (row[x] === "^") {
                         count += 1
-                        // console.log("SPLIT: %s to %s, %s", x, x - 1, x + 1)
-                        // beamIndices.push([x - 1, x + 1]);
                         newIndexs.push(x-1, x+1)
                     }
             })
-            const z = [...new Set(newIndexs)];
-            // count += z.length - beamIndices[i-1];
             beamIndices.push([...new Set(newIndexs)])
         }
-        // console.log({beamIndices})
     })
     return count;
 }
 
 function part2(input) {
-    return "";
+    const paths = Array(input[0].length).fill(0);
+    input.forEach((row) => {
+        row.split("").forEach((ele, i) => {
+            if(ele === 'S') {
+                paths[i] = 1;
+            }
+            if(ele === "^") {
+                paths[i-1] += paths[i]
+                paths[i+1] += paths[i]
+                paths[i] = 0;
+            }
+        })
+    })
+    return paths.reduce((acc, ele) => acc + ele)
 }
 
 
--- day7/test.js
+++ day7/test.js
@@ -27,7 +27,36 @@ test('Part1 - should handle base cases', async () => {
 
 test('Part1 - should handle full input', async () => {
     const input = await processInput("input.txt");
-    console.log({input})
     const result = part1(input)
     assert.equal(result, 1698);
 })
+
+test('Part2 - should handle base cases', async () => {
+    const input = [
+        ".......S.......",
+        "...............",
+        ".......^.......",
+        "...............",
+        "......^.^......",
+        "...............",
+        ".....^.^.^.....",
+        "...............",
+        "....^.^...^....",
+        "...............",
+        "...^.^...^.^...",
+        "...............",
+        "..^...^.....^..",
+        "...............",
+        ".^.^.^.^.^...^.",
+        "..............."
+    ];
+    const result = part2(input)
+    assert.equal(result, 40);
+})
+
+test('Part2 - should handle full input', async () => {
+    const input = await processInput("input.txt");
+    const result = part2(input)
+    assert.equal(result, 95408386769474);
+})
+
